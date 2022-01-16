855982771e27a5666c7cf031ebe2f02748cedfba
Etisha Jain <73745016+Etishajain17@users.noreply.github.com>
2022-01-17 02:38:20 +0530

    Time: 1 ms (69.78%), Space: 47.4 MB (18.44%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -5,6 +5,7 @@ class Solution {
         int[] ans=new int[n];
         for(int i=0;i<nums.length;i++)
         {
+            // best step
             ans[i]=ans[i+nums.length]=nums[i];
         }
         return ans;
