2d2ad33c143e477dee6763cb7ba140d3a573c6a6
Etisha Jain <73745016+Etishajain17@users.noreply.github.com>
2022-01-17 02:41:45 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (73.42%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] runningSum(int[] arr) 
+    {
+        for(int i=1;i<arr.length;i++)
+        {
+            arr[i]=arr[i]+arr[i-1];
+        } 
+        return arr;
+    }
+}
