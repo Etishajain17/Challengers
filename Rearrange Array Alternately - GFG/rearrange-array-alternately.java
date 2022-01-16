a824b1b24dae61d11f8e84394b671a975729d669
Etisha Jain <73745016+Etishajain17@users.noreply.github.com>
2022-01-17 03:44:35 +0530

    Added solution - LeetHub

---
+++ Rearrange Array Alternately - GFG/rearrange-array-alternately.java
@@ -0,0 +1,96 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args)throws IOException
+	{
+
+// 		Scanner in = new Scanner(System.in);
+        
+// 		int t = in.nextInt();
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+         //testcases
+        int t = Integer.parseInt(read.readLine());
+		
+		while(t-- >0)
+		{
+		    //size of array
+		    int n = Integer.parseInt(read.readLine());
+		    int[] temp= new int[n];
+		    
+		    String str[] = read.readLine().trim().split(" ");
+		    
+		    //adding elements to the array
+		    for(int i=0;i<n;i++)
+		        temp[i] = Integer.parseInt(str[i]);
+		    
+		    // StringBuffer sb = new StringBuffer();
+		    
+		    Solution ob = new Solution();
+		    
+		    //calling rearrange() function
+		    ob.rearrange(temp, n);
+		    StringBuffer sb = new StringBuffer();
+		    
+		    //appending and printing the elements
+		    for(int i =0; i < n; i++)
+		     sb.append(temp[i] + " ");
+		    System.out.println(sb);
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+class Solution{
+    
+    // temp: input array
+    // n: size of array
+    //Function to rearrange  the array elements alternately.
+    public static void rearrange(int arr[], int n){
+        
+        // Your code here
+        int low=0;
+        int high=n-1;
+        while(low<high)
+        {
+            int t=arr[low];
+            arr[low]=arr[high];
+            arr[high]=t;
+            low++;
+            high--;
+        }
+        int[] ar2=new int[n];
+        int j=0;
+        if(n==1)
+        {
+             ar2[0]=arr[j];
+        }
+        else
+        {
+            int x=0;
+            if(n%2!=0){
+                 x=n-1;
+                 ar2[x]=arr[(n/2)];}
+            else
+                 x=n;
+        for(int i=0;i<x;i=i+2)
+        {
+            ar2[i]=arr[j];
+            ar2[i+1]=arr[n-j-1];
+            j++;
+        }
+        }
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=ar2[i];
+        }
+    }
+    
+}
