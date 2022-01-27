2018d70176c3c58943390de15645fcd7db68031f
Etisha Jain <jainetisha0714@gmail.com>
2022-01-28 00:23:44 +0530

    Added solution - LeetHub

---
+++ Print 1 to n without using loops - GFG/print-1-to-n-without-using-loops.java
@@ -0,0 +1,35 @@
+// { Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int N=sc.nextInt();
+			
+            Solution ob = new Solution();
+            ob.printTillN(N);
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static void printTillN(int n)
+    {
+        // code here
+        if(n==0)
+          return;
+        printTillN(n-1);
+        System.out.print(n+" ");
+    }
+}
