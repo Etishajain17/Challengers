602a5eab07cad6b05352fcac192db73c6faf97e6
Etisha Jain <jainetisha0714@gmail.com>
2022-01-28 02:05:41 +0530

    Time: 2 ms (27.93%), Space: 41.9 MB (6.08%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.java
@@ -0,0 +1,16 @@
+class Solution
+{
+    public boolean isPowerOfTwo(int n) 
+    {
+        
+        if(n==1  )
+            return true;
+        else if(n==0)
+            return false;
+        
+        if(n%2==0)
+            return isPowerOfTwo(n/2);
+        
+        return false;
+    }
+}
