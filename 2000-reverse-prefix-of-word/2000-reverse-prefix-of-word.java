a521e6b93fb08ba3db6af72682aba29e46b88a7f
Etisha Jain <jainetisha0714@gmail.com>
2022-02-20 17:41:27 +0530

    Time: 28 ms (5.93%), Space: 43.1 MB (5.45%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        int l=0,h=word.indexOf(ch);
+        if(l<h)
+        word=word.charAt(h)+word.substring(l+1,h)+word.charAt(l)+word.substring(h+1);
+        l++;
+        h--;
+        while(l<h)
+        {
+            word=word.substring(0,l)+word.charAt(h)+word.substring(l+1,h)+word.charAt(l)+word.substring(h+1); 
+            l++;
+            h--;
+        }
+        return word;
+    }
+}
