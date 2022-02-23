0c498e21e53787a55941c281f6128ae67088e778
Etisha Jain <jainetisha0714@gmail.com>
2022-02-23 11:35:54 +0530

    Time: 1 ms (97.45%), Space: 48.4 MB (19.92%) - LeetHub

---
+++ 905-sort-array-by-parity/905-sort-array-by-parity.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int low=0,high=nums.length-1;
+        while(low<high)
+        {
+            if(nums[low]%2>nums[high]%2)
+            {
+                int t=nums[low];
+                nums[low]=nums[high];
+                nums[high]=t;
+            }
+            if(nums[low]%2==0) low++;
+            if(nums[high]%2==1) high--;
+        }
+        return nums;
+    }
+}
