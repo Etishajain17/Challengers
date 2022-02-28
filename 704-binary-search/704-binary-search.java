f9af074fa6970cfe8f3d31f2e836cbbf324b8076
Etisha Jain <jainetisha0714@gmail.com>
2022-02-28 19:13:56 +0530

    Time: 1 ms (16.03%), Space: 53.8 MB (35.06%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -4,5 +4,19 @@ class Solution {
         if(Arrays.binarySearch(nums,target)<0)
             return -1;
         return Arrays.binarySearch(nums,target);
+        
+        //**Approach**
+        // int first=0,last=nums.length-1;
+        // while(first<=last)
+        // {
+        //     int mid=(first+last)/2;
+        //     if(nums[mid]==target)
+        //         return mid;
+        //     else if(nums[mid]<target)
+        //         first=mid+1;
+        //     else
+        //         last=mid-1;
+        // }
+        // return -1;
     }
 }
