ae1c010556a8eb1a23a2d52f9247f98a9f085ddd
Etisha Jain <jainetisha0714@gmail.com>
2022-03-04 17:32:25 +0530

    Time: 1 ms (100.00%), Space: 43.8 MB (79.90%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.java
@@ -0,0 +1,17 @@
+class Solution 
+{
+    public void moveZeroes(int[] nums) 
+    {
+        int c=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]!=0)
+            {
+                int temp=nums[i];
+                nums[i]=nums[c];
+                nums[c]=temp;
+                c++;
+            }
+        }
+    }
+}
