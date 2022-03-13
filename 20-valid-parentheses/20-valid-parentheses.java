d671d44bd58ac4aa59a3af7c424fcb628e7776d4
Etisha Jain <jainetisha0714@gmail.com>
2022-03-13 12:54:34 +0530

    Time: 2 ms (81.09%), Space: 42.4 MB (18.20%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isValid(String s) 
+    {
+        Deque<Character> stack=new ArrayDeque<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char x=s.charAt(i);
+            if(x=='(' || x=='{' || x=='[')
+            {
+                stack.push(x);
+            }
+            else
+            {
+                if(stack.isEmpty()==true)
+                {
+                    return false;
+                }   
+                else if(isMatching(stack.peek(),x)==true)
+                    stack.pop();
+                else
+                    return false;  
+            }
+        }
+        return stack.isEmpty();
+    }
+    public boolean isMatching(char a,char b)
+    {
+        return((a=='(' && b==')') || (a=='{' && b=='}') || (a=='[' && b==']'));
+    }
+}
