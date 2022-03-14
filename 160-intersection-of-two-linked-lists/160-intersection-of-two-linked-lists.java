7a4d24b1d6a2b01f79a9757dda7034ded5028774
Etisha Jain <jainetisha0714@gmail.com>
2022-03-14 19:03:03 +0530

    Time: 2 ms (57.30%), Space: 54.9 MB (50.39%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -10,20 +10,46 @@
  * }
  */
 public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode tempA=headA,tempB=headB;
-        while(tempA!=null)
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) 
+    {
+        //Efficient Approach O(n+m) Time complexity and O(1) Space Complexity
+        ListNode tempA = headA,tempB = headB;
+        while (tempA != null) 
         {
-            tempB=headB;
-            while(tempB!=null)
-            {
-                if(tempA==tempB)
-                    return tempA;
-
-                tempB=tempB.next;
-            }
-            tempA=tempA.next;
+            tempA.val = -tempA.val;
+            tempA = tempA.next;
         }
-        return null;
+        ListNode inter = null;
+        while (tempB != null && inter == null) 
+        {
+            if (tempB.val < 0)
+                inter = tempB;
+
+            tempB = tempB.next;
+        }
+        tempA = headA;
+        while (tempA != null) 
+        {
+             tempA.val = -tempA.val;
+            tempA = tempA.next;
+        }
+        
+        return inter;
+        
+        //Naive Approach  O(nm) Time complexity and O(1) Space Complexity
+//         ListNode tempA=headA,tempB=headB;
+//         while(tempA!=null)
+//         {
+//             tempB=headB;
+//             while(tempB!=null)
+//             {
+//                 if(tempA==tempB)
+//                     return tempA;
+
+//                 tempB=tempB.next;
+//             }
+//             tempA=tempA.next;
+//         }
+//         return null;
     }
 }
