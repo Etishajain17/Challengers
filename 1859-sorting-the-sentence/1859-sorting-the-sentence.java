9168cd767db6a142f1d603144832b42991cd730f
Etisha Jain <jainetisha0714@gmail.com>
2022-03-15 20:26:24 +0530

    Time: 6 ms (20.87%), Space: 42.9 MB (7.63%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -0,0 +1,21 @@
+class Solution 
+{
+    public String sortSentence(String s) 
+    {
+        int j=0,i=0;
+        String[] str=s.split(" ");
+        for(i=0;i<s.length()-2;i++)
+        {
+            if(s.charAt(i+1)==' ')
+            {
+                str[Character.getNumericValue(s.charAt(i))-1]=s.substring(j,i);
+                j=i+2;
+            }
+        }
+        str[Character.getNumericValue(s.charAt(i+1))-1]=s.substring(j,i+1);
+        s="";
+        for(int x=0;x<str.length-1;x++)
+            s=s+str[x]+" ";
+        return s+str[str.length-1];
+    }
+}
