9cf7ca0efff2d437008ffdf9b24b696bb448b231
Etisha Jain <jainetisha0714@gmail.com>
2022-03-15 13:38:55 +0530

    Time: 1 ms (91.58%), Space: 45.2 MB (19.28%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char[] ca=new char[s.length()];
+        for(int i=0;i<ca.length;i++)
+            ca[indices[i]]=s.charAt(i);
+        return String.valueOf(ca);
+    }
+}
