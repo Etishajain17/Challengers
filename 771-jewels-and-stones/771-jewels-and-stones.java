b4871992b3e0caec26059e70dce0d62521c7344f
Etisha Jain <jainetisha0714@gmail.com>
2022-03-15 23:52:16 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (73.63%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.java
+++ 771-jewels-and-stones/771-jewels-and-stones.java
@@ -1,11 +1,11 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-     int count=0;
+     int c=0;
 
-    for(int i=0;i<stones.length();i++){
+     for(int i=0;i<stones.length();i++){
         if(jewels.indexOf(stones.charAt(i))!=-1) 
-            count++;
-        }
-        return count;
+            c++;
+     }
+     return c;
     }
 }
