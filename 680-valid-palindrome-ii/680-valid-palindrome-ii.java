dbb396e05388af1e342f02f47ede63f9764ea4dd
Etisha Jain <jainetisha0714@gmail.com>
2022-04-02 11:10:13 +0530

    Time: 7 ms (83.51%), Space: 54.6 MB (41.50%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        return occur(s, 0, s.length()-1, 1);
+    }
+    
+    public boolean occur(String s, int start, int end, int count) {
+        if(count == -1)
+            return false;
+        int low = start;
+        int high = end;
+        while(low < high) {
+            if(s.charAt(low) != s.charAt(high)) 
+                return occur(s, low+1, high, count-1) || occur(s, low, high-1, count-1); 
+            low++;
+            high--;
+        }
+        return true;
+    }
+}
