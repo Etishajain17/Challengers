18d9ff338d581874c433ff1c86c5c84877445313
Etisha Jain <jainetisha0714@gmail.com>
2022-04-03 17:22:27 +0530

    Time: 2 ms (33.36%), Space: 41.7 MB (79.70%) - LeetHub

--- 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.java
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.java
@@ -4,9 +4,7 @@ class Solution {
         for(int i=0;i<s.length();i++)
         {
             if(Character.isDigit(s.charAt(i)))
-            {
                 s1.append((char)(s.charAt(i-1) + Character.getNumericValue(s.charAt(i))));
-            }
             else
                 s1.append(s.charAt(i));
         }
