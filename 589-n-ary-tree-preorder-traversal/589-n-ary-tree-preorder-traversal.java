8978052834ad2184730dc38e94864e2e8c534b0a
Etisha Jain <jainetisha0714@gmail.com>
2022-04-03 17:25:12 +0530

    Time: 0 ms (100.00%), Space: 43 MB (89.48%) - LeetHub

--- 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.java
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.java
@@ -23,7 +23,6 @@ class Solution {
         if(root==null)
             return l;
         l.add(root.val);
-        
         for(Node child:root.children)
             l=preorder(child);
         return l;
