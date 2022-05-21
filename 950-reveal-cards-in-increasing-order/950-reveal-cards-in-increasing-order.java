83f1e2e01dd92f6d8689521f7f80891c1c873a78
Etisha Jain <jainetisha0714@gmail.com>
2022-05-21 13:46:27 +0530

    Time: 5 ms (46.15%), Space: 44.6 MB (20.26%) - LeetHub

---
+++ 950-reveal-cards-in-increasing-order/950-reveal-cards-in-increasing-order.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        Queue<Integer> queue=new LinkedList<>();
+        Arrays.sort(deck);
+        for(int i=deck.length-1;i>=0;i--)
+        {
+            if(!queue.isEmpty())
+            {
+                queue.add(queue.remove());
+                queue.add(deck[i]);
+            }
+            else
+                queue.add(deck[i]);
+        }
+        int i=deck.length;
+        while(i-->0){
+            deck[i]=queue.remove();
+            // i--;
+        }
+        return deck;
+    }
+}
