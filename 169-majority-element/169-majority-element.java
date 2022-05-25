60f47d35cf551a3d1cb87db1db9f36bc9c7b6603
Etisha Jain <jainetisha0714@gmail.com>
2022-05-25 17:03:29 +0530

    Time: 2 ms (81.73%), Space: 56.1 MB (46.01%) - LeetHub

---
+++ 169-majority-element/169-majority-element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int res=0,count=1;
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[res]==nums[i])
+                count++;
+            else
+                count--;
+            if(count==0){
+                count=1;
+                res=i;
+            }
+        }
+        return nums[res];
+    }
+}
