d43847251a77cdb145bdd253cf9f47b941fdf479
Etisha Jain <jainetisha0714@gmail.com>
2022-05-26 12:51:22 +0530

    Time: 0 ms (100.00%), Space: 41 MB (89.26%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        for(int i=0;i<matrix.length;i++)
+        {
+            for(int j=i;j<matrix.length;j++)
+            {
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+            
+        }
+        for(int i=0;i<matrix.length;i++)
+        {
+            reverse(matrix[i]);
+        }
+    }
+    public void reverse(int[] arr)
+    {
+        int lo=0,hi=arr.length-1;
+        while(lo<hi)
+        {
+            int t=arr[lo];
+            arr[lo]=arr[hi];
+            arr[hi]=t;
+            lo++;hi--;
+        }
+    }
+}
