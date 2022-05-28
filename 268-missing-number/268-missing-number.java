f2e6f4573fed483ca0d73cb5017d5caf5b3422f8
Etisha Jain <jainetisha0714@gmail.com>
2022-05-28 22:08:00 +0530

    Time: 0 ms (100.00%), Space: 52 MB (10.55%) - LeetHub

---
+++ 268-missing-number/268-missing-number.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int s=(nums.length*(nums.length+1))/2,sum=0;
+        for(int i=0;i<nums.length;i++)
+            sum+=nums[i];
+        return s-sum;
+    }
+}
