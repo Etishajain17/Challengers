341275598fc7d082ceb24df3a99ab4fd6021d235
Etisha Jain <jainetisha0714@gmail.com>
2022-05-28 13:52:34 +0530

    Time: 1 ms (60.05%), Space: 40.5 MB (79.78%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle.length()==0)
+            return 0;
+        for(int i=0;i<haystack.length();i++){
+            if(haystack.charAt(i)==needle.charAt(0)){
+                int j=i,k=0;
+                while(i<haystack.length() && k<needle.length()
+                     && haystack.charAt(i)==needle.charAt(k)){
+                    i++;
+                    k++;
+                }
+                if(k==needle.length())
+                    return j;
+                else{
+                    i=j;
+                    k=0;
+                }
+            }
+        }
+        return -1;
+    }
+}
