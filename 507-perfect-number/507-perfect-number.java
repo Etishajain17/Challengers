73da3a9f7da22268923582136d9fa212f1d52dd0
Etisha Jain <jainetisha0714@gmail.com>
2022-05-31 00:29:49 +0530

    Time: 2 ms (73.17%), Space: 40.5 MB (66.96%) - LeetHub

---
+++ 507-perfect-number/507-perfect-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int sum=0;
+        for(int i=1;i*i<=num;i++){
+            if(num%i==0){
+                sum+=i;
+                if((i*i)!=num)
+                    sum+=num/i;
+            }
+        }
+        return sum-num==num;
+    }
+}
