6d9a815f8636e2769b524cdfb9f67b34c92cea41
Etisha Jain <jainetisha0714@gmail.com>
2022-08-28 17:04:21 +0530

    Time: 1 ms (99.45%), Space: 45.7 MB (62.86%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -1,21 +1,46 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-        int c=0;
-        int[] arr1=new int[arr.length];
-        for(int i=0;i<arr.length && c<arr.length;i++){
-            
-            arr1[c]=arr[i];
-            c++;
-            if(c>=arr.length)
-                break;
-            if(arr[i]==0){
-                arr1[c]=0;
-                c++;
-                
-            } 
-            
+        
+        int eli= arr.length-1,  cli = eli ;
+        
+        for (int l=0 ; l <= eli ; l++){
+            if (arr[l]==0){
+                if(eli==l){
+                    arr[cli--]=0;                
+                }
+                eli--;
+            }
         }
-        for(int i=0;i<arr.length;i++)
-            arr[i]=arr1[i];
+        
+        for(int i = cli, j = eli ; j >= 0; j--){
+            arr[i--] = arr[j];  
+            if (arr[j]==0){
+                arr[i--] = arr[j];
+            }
+        }        
     }
 }
+
+
+
+// class Solution {
+//     public void duplicateZeros(int[] arr) {
+//         int c=0;
+//         int[] arr1=new int[arr.length];
+//         for(int i=0;i<arr.length && c<arr.length;i++){
+            
+//             arr1[c]=arr[i];
+//             c++;
+//             if(c>=arr.length)
+//                 break;
+//             if(arr[i]==0){
+//                 arr1[c]=0;
+//                 c++;
+                
+//             } 
+            
+//         }
+//         for(int i=0;i<arr.length;i++)
+//             arr[i]=arr1[i];
+//     }
+// }
