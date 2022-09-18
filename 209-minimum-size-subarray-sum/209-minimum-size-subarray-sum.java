e2215f5b4c9b020ba8930e117f1df5c7aaf90547
Etisha Jain <jainetisha0714@gmail.com>
2022-09-18 23:02:05 +0530

    Time: 3 ms (35.88%), Space: 58.1 MB (39.59%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
@@ -3,12 +3,12 @@ class Solution {
         int si=0,ei=0,sum=0,ans=Integer.MAX_VALUE;
         while(ei<nums.length){
             sum+=nums[ei];
-            while(si<=ei && sum>=target){
-                ans=Math.min(ans,ei-si+1);
-                System.out.println(ei-si+" "+ans);
+            while(sum-nums[si]>=target){
                 sum-=nums[si];
                 si++;
             }
+            if(sum>=target)
+                ans=Math.min(ans,ei-si+1);
             ei++;
         }
         return ans==Integer.MAX_VALUE?0:ans;
